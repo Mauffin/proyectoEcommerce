03b548842f9ac029dd032ac3787ba58b76593a76
mauffin <damiancardenas22@gmail.com>
2023-06-04 21:01:33 -0400

    feat:added git ignore in main folder

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const app = express();
 
-app.use(express.json(),express.urlencoded({exrended:true}));
+app.use(express.json(),express.urlencoded({extended:true}));
 
 app.use(
     cors({
